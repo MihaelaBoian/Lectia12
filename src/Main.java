1afb200c301e590ec8b302dfdbee216807cd1ac1
MihaelaBoian <pinzari_m@mail.ru>
2023-10-31 16:55:16 +0200

    M

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 //Exercitiu 1
-//     Programmer programmer = new Programmer("Vasile","068545785","Stefan Voda");
-//     programmer.attendTraining();
-//
-//     Manager manager= new Manager("Ana","0586542556","Soroca");
-//     manager.attendTraining();
-//     manager.conductInterview();
-//     System.out.println();
-//     System.out.println("--------------------------------");
+     Programmer programmer = new Programmer("Vasile","068545785","Stefan Voda");
+     programmer.attendTraining();
+
+     Manager manager= new Manager("Ana","0586542556","Soroca");
+     manager.attendTraining();
+     manager.conductInterview();
+     System.out.println();
+     System.out.println("--------------------------------");
 
 //     Exercitiu 2:
       Training t1 = new Programmer("Stefan  ","025485555   ","Chisinau   ");
